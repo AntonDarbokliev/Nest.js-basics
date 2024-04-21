5573a74635832ecf2e880ae11e4ba8d9e3d76680
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-21 13:16:46 +0300

    setup

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [AuthModule, BookmarkModule, UserModule],
+})
+export class AppModule {}
