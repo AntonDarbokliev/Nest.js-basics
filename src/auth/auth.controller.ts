3e9655a2a729a993ec59871c07dd76c6e089faa6
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-22 22:07:57 +0300

    auth controller/service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp() {
-    return this.authService.signup();
+  async signUp(@Body() dto: AuthDto) {
+    const user = await this.authService.signup(dto);
+    return user;
   }
 
   @Post('signin')
-  signIn() {
-    return this.authService.signin();
+  async signIn(@Body() dto: AuthDto) {
+    return await this.authService.signin(dto);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserSchema } from 'src/user/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+        signOptions: { expiresIn: '10m' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
