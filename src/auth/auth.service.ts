b33001758b9208b78ace697859560a1e6383eb6a
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-22 22:08:04 +0300

    auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from 'src/user/user.schema';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signin() {
-    return {
-      message: 'I have signed in',
-    };
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtServce: JwtService,
+  ) {}
+
+  async signin(dto: AuthDto) {
+    try {
+      const existingUser = await this.userModel.findOne({ email: dto.email });
+      if (!existingUser) {
+        throw new ForbiddenException("User doesn't exist");
+      }
+
+      const isPasswordCorrect = bcrypt.compare(dto.password, existingUser.hash);
+
+      if (!isPasswordCorrect) {
+        throw new ForbiddenException('Wrong password');
+      }
+      const payload = {
+        sub: existingUser.id,
+        email: existingUser.email,
+      };
+      return {
+        access_token: await this.jwtServce.signAsync(payload),
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 
-  signup() {
-    return {
-      message: 'I have signed up',
-    };
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await bcrypt.hash(dto.password, 10);
+      const user = await this.userModel.create({
+        email: dto.email,
+        hash,
+      });
+
+      const payload = {
+        sub: user.id,
+        email: user.email,
+      };
+      return {
+        access_token: await this.jwtServce.signAsync(payload),
+      };
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ForbiddenException('Credentials taken');
+      } else {
+        throw error;
+      }
+    }
   }
 }
