979cc62cf848ecf8555feffce142da32d8d6390f
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-22 22:08:51 +0300

    adjustments

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2018
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
