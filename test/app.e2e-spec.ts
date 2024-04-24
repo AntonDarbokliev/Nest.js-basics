3e2d422c7e00dfc38e381ef2a1ce0252803dd2fa
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-24 20:38:47 +0300

    e2e testing (in progress)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,30 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 
-describe('AppController (e2e)', () => {
+// TODO: Make a testing db enviorment, a test db and .env var are already created
+// Currently watcing this video => https://www.youtube.com/watch?v=R2ndY_JhxWk 9:00 min
+
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
-
-    app = moduleFixture.createNestApplication();
+    })
+      .overrideProvider('MONGO_TEST_URI')
+      .compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(() => {
+    app.close();
   });
+
+  it.todo('Should pass');
 });
