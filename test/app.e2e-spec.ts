398691b519514b3d8bb5b46964b220e52307e54e
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-25 18:41:36 +0300

    set up more testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,18 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-
+import { MongooseHelperService } from '../src/mongoose/mongooseHelper.service';
+// import { config } from 'dotenv';
 // TODO: Make a testing db enviorment, a test db and .env var are already created
 // Currently watcing this video => https://www.youtube.com/watch?v=R2ndY_JhxWk 9:00 min
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let mongooseService: MongooseHelperService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider('MONGO_TEST_URI')
-      .compile();
+    }).compile();
     app = moduleRef.createNestApplication();
     app.useGlobalPipes(
       new ValidationPipe({
@@ -20,6 +20,8 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    mongooseService = app.get(MongooseHelperService);
+    await mongooseService.dropDB();
   });
 
   afterAll(() => {
