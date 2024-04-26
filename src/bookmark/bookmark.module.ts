d31b749a055b17fb3835041a207c8272e8031c9e
Anton Darbokliev <georgedarbokliev@gmail.com>
2024-04-26 18:05:21 +0300

    bookmark module

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,4 +1,16 @@
 import { Module } from '@nestjs/common';
+import { BookmarkController } from './bookmark.controller';
+import { BookmarkService } from './bookmark.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Bookmark, BookmarkSchema } from './bookmark.schema';
 
-@Module({})
+@Module({
+  controllers: [BookmarkController],
+  providers: [BookmarkService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Bookmark.name, schema: BookmarkSchema },
+    ]),
+  ],
+})
 export class BookmarkModule {}
